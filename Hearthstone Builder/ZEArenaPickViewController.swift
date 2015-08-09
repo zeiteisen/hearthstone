240668677f1b092bb43ccdb404fa356e6072a56a
Hanno Bruns <hanno.bruns@googlemail.com>
2015-08-09 16:11:21 +0200

    add first arena draft test data

--- Hearthstone Builder/ZEArenaPickViewController.swift
+++ Hearthstone Builder/ZEArenaPickViewController.swift
@@ -1,9 +1,35 @@
 import UIKit
 
 class ZEArenaPickViewController: UIViewController {
-
+    @IBOutlet weak var firstCardButton: SmartButton!
+    @IBOutlet weak var secondCardButton: SmartButton!
+    @IBOutlet weak var thirdCardButton: SmartButton!
+    
+    private var drafts: NSArray!
+    private var currentIndex = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let quizzes = NSArray(contentsOfFile: NSBundle.mainBundle().pathForResource("ArenaDrafts", ofType: "plist")!)!
+        let firstQuiz = quizzes[0] as! NSDictionary
+        drafts = firstQuiz["draft"] as! NSArray
+        updateCardsWithCurrentIndex(currentIndex)
+    }
+    
+    func imageFromName(name: String) -> UIImage {
+        let imageName = "\(name).jpg"
+        return UIImage(named: imageName)!
+    }
+    
+    func updateCardsWithCurrentIndex(index: Int) {
+        let draft = drafts[index] as! NSDictionary
+        let first = draft["1"] as! String
+        let second = draft["2"] as! String
+        let third = draft["3"] as! String
+//        let pick = draft["pick"] as! String
+        firstCardButton.setImage(imageFromName(first), forState: .Normal)
+        secondCardButton.setImage(imageFromName(second), forState: .Normal)
+        thirdCardButton.setImage(imageFromName(third), forState: .Normal)
     }
     
     override func viewWillAppear(animated: Bool) {
@@ -19,4 +45,24 @@ class ZEArenaPickViewController: UIViewController {
         appDelegate.onlyPortrait = true
         UIDevice.currentDevice().setValue(UIInterfaceOrientation.Portrait.rawValue, forKey: "orientation")
     }
+    
+    func showNext() {
+        if drafts.count > currentIndex + 1 {
+            currentIndex++
+            updateCardsWithCurrentIndex(currentIndex)
+        }
+    }
+    
+    @IBAction func firstTouched(sender: AnyObject) {
+        showNext()
+    }
+    
+    @IBAction func secondTouched(sender: AnyObject) {
+        showNext()
+    }
+    
+    @IBAction func thirdTouched(sender: AnyObject) {
+        showNext()
+    }
+    
 }
